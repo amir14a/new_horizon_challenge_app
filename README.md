1713077b053158b172c4cf39eba84e896a29115b
AmirAbbas-PC <a.abbasj@yahoo.com>
2024-10-07 03:22:15 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# new_horizon_challenge_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/new_horizon_challenge_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.new_horizon_challenge_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
