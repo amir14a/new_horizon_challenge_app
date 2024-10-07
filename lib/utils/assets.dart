e933cda0999ad481bbea1c27163f7ebf4814f26b
AmirAbbas-PC <a.abbasj@yahoo.com>
2024-10-07 06:35:25 +0330

    Add required assets and icons

---
+++ lib/utils/assets.dart
@@ -0,0 +1,5 @@
+const CALENDER_ICON = 'assets/svg/calender_icon.svg';
+const MENU_ICON = 'assets/svg/menu_icon.svg';
+const SEARCH_ICON = 'assets/svg/search_icon.svg';
+const USER_ICON = 'assets/svg/user_icon.svg';
+const USERS_GROUP_ICON = 'assets/svg/users_group_icon.svg';
