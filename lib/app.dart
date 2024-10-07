936a5320038e65925d0c35cb11b1b16312a3f378
AmirAbbas-PC <a.abbasj@yahoo.com>
2024-10-07 06:44:29 +0330

    Create theme and assign app font and add drag devices and change app to rtl

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,25 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:new_horizon_challenge_app/presentation/screens/home/home.dart';
+import 'package:new_horizon_challenge_app/utils/themes.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomePage());
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: MaterialApp(
+        home: HomePage(),
+        theme: mainTheme,
+        scrollBehavior: ScrollConfiguration.of(context).copyWith(dragDevices: {
+          PointerDeviceKind.touch,
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.trackpad,
+        }),
+      ),
+    );
   }
 }
---
+++ lib/utils/themes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final mainTheme = ThemeData(
+  fontFamily: 'Vazirmatn',
+  useMaterial3: true,
+);
