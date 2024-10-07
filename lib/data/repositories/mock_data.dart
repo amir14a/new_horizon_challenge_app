51ff9225d96e69d49252939077833ee23258b58d
AmirAbbas-PC <a.abbasj@yahoo.com>
2024-10-07 03:52:32 +0330

    Create mock data

---
+++ lib/data/repositories/mock_data.dart
@@ -0,0 +1,51 @@
+import 'package:new_horizon_challenge_app/data/models/loan_box_model.dart';
+import 'package:new_horizon_challenge_app/data/models/location_address_model.dart';
+
+abstract class MockData {
+  static final mockLoanBoxes = [
+    LoanBox(
+      id: 1,
+      title: 'صندوق وام احسان',
+      secondTitle: 'صندوق قرعه کشی',
+      totalAmount: 5000000,
+      position: 'مدیر',
+      membersCount: 12,
+      billingDate: '2024-11-07 00:00:00',
+    ),
+    LoanBox(
+      id: 2,
+      title: 'صندوق وام امیر',
+      secondTitle: 'صندوق خانوادگی',
+      totalAmount: 20000000,
+      position: 'شرکت کننده',
+      membersCount: 20,
+      billingDate: '2024-11-02 00:00:00',
+    ),
+    LoanBox(
+      id: 3,
+      title: 'صندوق وام شرکت',
+      secondTitle: 'صندوق قرعه کشی کارمندی',
+      totalAmount: 10000000,
+      position: 'دستیار',
+      membersCount: 7,
+      billingDate: '2024-12-15 00:00:00',
+    ),
+  ];
+  static final mockAddresses = [
+    LocationAddress(
+      id: 1,
+      fullAddress: 'تهران، محله 22، برج آزادی، پلاک 15',
+      shortAddress: 'محله 22، تهران',
+    ),
+    LocationAddress(
+      id: 1,
+      fullAddress: 'اصفهان، میدان امام خمینی (ره)، روبروی عمارت عالی‌قاپو، پلاک 37',
+      shortAddress: 'میدان امام خمینی، اصفهان',
+    ),
+    LocationAddress(
+      id: 1,
+      fullAddress: 'مشهد، منطقه 14، کنارگذر حرم مطهر، پلاک 8',
+      shortAddress: 'منطقه 14، مشهد',
+    ),
+  ];
+}
